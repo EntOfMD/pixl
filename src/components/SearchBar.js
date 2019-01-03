40c93810b353fa95cfacd2ed2d6d9e0c52154e29
Ashell Fernando <ashellz2010@gmail.com>
2019-01-03 10:51:56 -0500

    any change to text field will be console logged to experiment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	onInputChange(event) {
+		console.log(event.target.value);
+	}
+
 	render() {
 		return (
 			<div className="ui segment">
 				<div className="ui form">
-					<input className="field" type="text" placeholder="Type a keyword to search!" />
+					<input
+						className="field"
+						type="text"
+						placeholder="Type a keyword to search!"
+						onChange={this.onInputChange}
+					/>
 				</div>
 			</div>
 		);
