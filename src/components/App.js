6189c68fb2ed3501471d7b50c2bfc371157bda45
Ashell Fernando <ashellz2010@gmail.com>
2019-01-06 15:37:31 -0500

    updated to ES6 async await SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,29 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchSubmit(term) {
+	state = { images: [] };
+
+	async onSearchSubmit(term) {
 		//props flow downstream to child from parent. this function helps break that and bring props to parent from child
 
-		axios
-			.get(`https://api.unsplash.com/search/photos`, {
-				params: {
-					query: term,
-				},
-				headers: {
-					Authorization: `Client-ID 7446b82693cf5bb8dbdaac4d26fb93c3f47187ba711ad00b3c8f66b7323a56aa`,
-				},
-			})
-			.then((res) => {
-				console.log(res.data.results);
-			});
+		const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+			params: {
+				query: term,
+			},
+			headers: {
+				Authorization: `Client-ID 7446b82693cf5bb8dbdaac4d26fb93c3f47187ba711ad00b3c8f66b7323a56aa`,
+			},
+		});
+
+		this.setState({ images: response.data.results });
 	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '1vh' }}>
 				{' '}
-				<SearchBar onSubmit={this.onSearchSubmit} />
+				<SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+				<h2 className="ui header imageLength">Found: {this.state.images.length} images</h2>
 			</div>
 		);
 	}
