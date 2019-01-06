00bdda62373da84f50e77fdaaac1fddc90ddc8d3
Ashell Fernando <ashellz2010@gmail.com>
2019-01-06 14:42:14 -0500

    added axios GET request searchbar submit component

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,18 @@ class App extends React.Component {
 	onSearchSubmit(term) {
 		//props flow downstream to child from parent. this function helps break that and bring props to parent from child
 
-		axios.get(`https://api.unsplash.com/search/photos`, {});
+		axios
+			.get(`https://api.unsplash.com/search/photos`, {
+				params: {
+					query: term,
+				},
+				headers: {
+					Authorization: `Client-ID 7446b82693cf5bb8dbdaac4d26fb93c3f47187ba711ad00b3c8f66b7323a56aa`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data.results);
+			});
 	}
 
 	render() {
