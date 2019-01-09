2c99db8945377c2163c00529f09e0c7cef5ce4f7
Ashell Fernando <ashellz2010@gmail.com>
2019-01-09 11:18:26 -0500

    added key:id to get rid of warning

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const imageList = (props) => {
-	const images = props.images.map((image) => {
-		return <img src={image.urls.regular} alt={image.description} />;
+	const images = props.images.map(({ description, id, urls }) => {
+		//you can specify which nodes you want with a curly braces
+		return <img key={id} src={urls.regular} alt={description} />;
 	});
 
 	return <div>{images}</div>;
