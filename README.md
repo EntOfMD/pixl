52fcd1303e957e6bb95610d4ba96d529c599b2a2
Ashell Fernando <ashellz2010@gmail.com>
2019-01-10 09:55:47 -0500

    added dev config

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Type a keyword and generate a picture!
+
+_note_
+this project is done, I won't be updating it, but feel free to fork or whatnot. There is a huge possibilty with cheerio ;)
--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { unsplashKey } from '../config/dev.json';
 
 export default axios.create({
 	baseURL: 'https://api.unsplash.com',
 	headers: {
-		Authorization: `Client-ID 7446b82693cf5bb8dbdaac4d26fb93c3f47187ba711ad00b3c8f66b7323a56aa`,
+		Authorization: `Client-ID ${unsplashKey}`,
 	},
 });
